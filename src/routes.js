790ae62d1569450f5c54027a1641835db74d0aa8
HENRIQUE-CH <ch.henriquevinha@gmail.com>
2021-03-19 03:14:57 -0300

    Add arqs views

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,20 @@ const ProductController = require("./controllers/ProductController");
 const ClientController = require("./controllers/ClientController");
 const OrderController = require("./controllers/OrderController");
 
+//Pagina Inicial
+routes.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+//rptas produtos
 routes.get("/products", ProductController.index);
 routes.post("/product", ProductController.store);
 
+//rotas clientes
 routes.get("/clients", ClientController.index);
 routes.post("/client", ClientController.create);
 
+//rotas pedidos
 routes.get("/orders", OrderController.index);
 routes.post("/order", OrderController.store);
 
